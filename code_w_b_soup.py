2967c95f41edf8391aa39c9d6d768ce8f3220079
jeffslutzky <jeffslutzky@gmail.com>
2017-01-17 15:45:32 -0500

    Working

--- code_w_b_soup.py
+++ code_w_b_soup.py
@@ -1,7 +1,8 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 import urllib2
-import requests
 import pdb
+import csv
+import sys
 
 cik = "0001166559"
 
@@ -14,11 +15,52 @@ list_of_all_13f_links = []
 
 all_td = html.find_all("td")
 
-for element in all_td:
-    if "13F" in element.text:
-        list_of_all_13f_links.append(element.next_sibling.next_sibling.a.get("href"))
+# don't delete this!
+#
+# def get_details(link):
+#     detail_url = "https://www.sec.gov{}".format(link)
+#     detail_page = urllib2.urlopen(detail_url)
+#     detail_html = BeautifulSoup(detail_page, "lxml")
+#     for anchor in detail_html.find_all("a"):
+#         url = "https://www.sec.gov{}".format(anchor.get("href"))
+#         if ".txt" in url:
+#             txt = urllib2.urlopen(url)
+#             xml = BeautifulSoup(txt, "xml").xml
+#             # print xml
+#
+#
+# for element in all_td:
+#     if "13F" in element.text:
+#         list_of_all_13f_links.append(element.next_sibling.next_sibling.a.get("href"))
+#
+# for link in list_of_all_13f_links:
+#     get_details(link)
 
-for link in list_of_all_13f_links:
-    detail_url = "https://www.sec.gov{}".format(link)
-    detail_page = urllib2.urlopen(detail_url)
-    detail_html = BeautifulSoup(detail_page, "lxml")
+test_txt_url = "https://www.sec.gov/Archives/edgar/data/1166559/000110465916156931/0001104659-16-156931.txt"
+
+txt = urllib2.urlopen(test_txt_url)
+xml = BeautifulSoup(txt, "xml").XML
+
+
+def convert_to_csv(xml):
+    data = []
+    cols = []
+    for tag in xml.find_all(True, recursive=False):
+        # print tag.name
+        d = {}
+        for child in tag:
+            if hasattr(child, "name") and not isinstance(child, NavigableString):
+                d[child.name] = child.text
+                # print "{} = {}".format(d[child.name], child.text)
+        data.append(d)
+        cols = d.keys()
+        cw = csv.writer(open("result.csv",'w'))
+        # cw.writerow(data)
+        for row in data:
+            cw.writerow([row.get(k, 'N/A') for k in cols])
+
+convert_to_csv(xml)
+
+
+
+# print xml
---
+++ txt_13f.xsl
@@ -0,0 +1,17 @@
+<?xml version="1.0">
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="text" />
+
+<xsl:template match="/">
+<xsl:for-each>
+  
+</xsl:for-each>
+</xsl:template>
+
+<xsl:template match="headerData"></xsl:template>
+
+
+<xsl:template match="formData"></xsl:template>
+
+</xsl:stylesheet>
